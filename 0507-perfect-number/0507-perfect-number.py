2572490b1c27c48f9e85af008985d58691879901
Charan Yelimela <121748826+charanYelimela334@users.noreply.github.com>
2026-01-21 23:53:26 +0530

    Time: 0 ms (100%), Space: 12.6 MB (48.83%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def checkPerfectNumber(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        return num in {6, 28, 496, 8128, 33550336}
+        
